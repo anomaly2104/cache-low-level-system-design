e97b8f103ebc16bbf47113e975a0b35aa5765bf9
Udit Agarwal <udagarw@microsoft.com>
2020-07-19 21:15:55 +0530

    Add implementation for HashMap based storage

--- src/main/java/com/uditagarwal/cache/storage/HashMapBasedStorage.java
+++ src/main/java/com/uditagarwal/cache/storage/HashMapBasedStorage.java
@@ -3,18 +3,25 @@ package com.uditagarwal.cache.storage;
 import com.uditagarwal.cache.exceptions.NotFoundException;
 import com.uditagarwal.cache.exceptions.StorageFullException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapBasedStorage<Key, Value> implements Storage<Key, Value> {
+
+    Map<Key, Value> mp = new HashMap<>();
 
     @Override
     public void add(Key key, Value value) throws StorageFullException {
+        mp.put(key, value);
     }
 
     @Override
     public void remove(Key key) throws NotFoundException {
+        mp.remove(key);
     }
 
     @Override
     public Value get(Key key) throws NotFoundException {
-        return null;
+        return mp.get(key);
     }
 }
--- src/main/java/com/uditagarwal/cache/storage/Storage.java
+++ src/main/java/com/uditagarwal/cache/storage/Storage.java
@@ -5,6 +5,8 @@ import com.uditagarwal.cache.exceptions.StorageFullException;
 
 public interface Storage<Key, Value> {
     public void add(Key key, Value value) throws StorageFullException;
+
     void remove(Key key) throws NotFoundException;
+
     Value get(Key key) throws NotFoundException;
 }
